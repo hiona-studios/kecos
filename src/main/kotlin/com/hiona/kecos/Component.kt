70b4fb363ad7d33d860866e6217904b9f5007df2
chiara-jm <ARG.volvo.swe>
2019-10-14 09:27:06 +0200

    iterate over values and entities of Components

--- src/main/kotlin/com/hiona/kecos/Component.kt
+++ src/main/kotlin/com/hiona/kecos/Component.kt
@@ -5,6 +5,7 @@ class Component<T> {
     private val values = mutableMapOf<Entity, T>()
 
     operator fun get(entity: Entity): T? = values[entity]
+
     operator fun set(entity: Entity, value: T?) {
         when (value) {
             null -> values.remove(entity)
@@ -13,4 +14,21 @@ class Component<T> {
     }
 
     infix fun at(entity: Entity): T? = values[entity]
+
+    fun forEachEntity(action: (Entity) -> Unit) {
+        values.keys.forEach(action)
+    }
+
+    fun forEachValue(action: (T) -> Unit) {
+        values.values.forEach(action)
+    }
+
+    fun forEach(action: (Entity, T) -> Unit) {
+        values.forEach(action)
+    }
+
+    fun <R> map(transform: (Entity, T) -> R): List<R> = values.map { (e, v) -> transform(e, v) }
+
 }
+
+typealias Tag = Component<Unit>
